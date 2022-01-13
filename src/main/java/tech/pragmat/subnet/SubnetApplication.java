314fe7882b49fc7235ca737df27fa37ab82d38d7
Karr1g4n <licherep803@gmail.com>
2022-01-13 15:15:12 +0200

    created opportunity add ip in DB and get ip from DB

--- src/main/java/tech/pragmat/subnet/SubnetApplication.java
+++ src/main/java/tech/pragmat/subnet/SubnetApplication.java
@@ -2,8 +2,10 @@ package tech.pragmat.subnet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SubnetApplication {
 
     public static void main(String[] args) {
