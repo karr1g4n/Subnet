bedfa823d7d33dfe0c9095e4d14e3fbaf92460a4
Karr1g4n <licherep803@gmail.com>
2022-01-13 15:10:08 +0200

    added query

--- src/main/java/tech/pragmat/subnet/repository/IpRepository.java
+++ src/main/java/tech/pragmat/subnet/repository/IpRepository.java
@@ -1,11 +1,13 @@
 package tech.pragmat.subnet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tech.pragmat.subnet.model.SuspectIp;
 
 @Repository
 public interface IpRepository extends JpaRepository<SuspectIp, Integer> {
 
-    SuspectIp getByStartLimit(int id);
+    @Query(value = "select * from  black_list u where ?1>=u.upper_bound  and ?1<=u.lower_bound", nativeQuery = true)
+    SuspectIp getSuspectIpBy(Integer id);
 }
