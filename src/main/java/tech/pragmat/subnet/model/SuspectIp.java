2c8343eb11bbcab94b0df0c81bf3215198930d1d
Karr1g4n <licherep803@gmail.com>
2022-02-11 14:36:09 +0200

    changed type for id in table black_list;
    changed GeneratedValue from AUTO to IDENTITY for correct work with date base

--- src/main/java/tech/pragmat/subnet/model/SuspectIp.java
+++ src/main/java/tech/pragmat/subnet/model/SuspectIp.java
@@ -21,7 +21,7 @@ import javax.persistence.Table;
 public class SuspectIp {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "upper_ip")
     private String upperIp;
